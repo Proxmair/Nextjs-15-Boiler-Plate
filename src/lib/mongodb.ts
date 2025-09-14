39703cf6317652804b79a558f5c3c55ac9d6b655
Proxmair <umair.xmair@gmail.com>
2025-09-14 21:12:51 +0500

    fix: remove type errors

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -3,24 +3,32 @@ import mongoose from "mongoose";
 const MONGODB_URI = process.env.MONGODB_URI as string;
 
 if (!MONGODB_URI) {
-  throw new Error("⚠️ Please define the MONGODB_URI environment variable inside .env.local");
+    throw new Error("⚠️ Please define the MONGODB_URI environment variable inside .env.local");
 }
 
-let cached = (global as any).mongoose;
+declare global {
+    // eslint-disable-next-line no-var
+    var mongoose: { conn: mongoose.Connection | null; promise: Promise<mongoose.Connection> | null } | undefined;
+}
+
+let cached = global.mongoose;
 
 if (!cached) {
-  cached = (global as any).mongoose = { conn: null, promise: null };
+    cached = global.mongoose = { conn: null, promise: null };
 }
 
 async function dbConnect() {
-  if (cached.conn) return cached.conn;
+    if (!cached) {
+        throw new Error("Cached mongoose connection is undefined.");
+    }
+    if (cached.conn) return cached.conn;
 
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => mongoose);
-  }
+    if (!cached.promise) {
+        cached.promise = mongoose.connect(MONGODB_URI).then((mongooseInstance) => mongooseInstance.connection);
+    }
 
-  cached.conn = await cached.promise;
-  return cached.conn;
+    cached.conn = await cached.promise;
+    return cached.conn;
 }
 
 export default dbConnect;
